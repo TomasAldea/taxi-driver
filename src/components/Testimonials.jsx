ca7d31734968a341baa8bc32d75db3e5b05aa3b4
TomasAldea <tomas.sbd.5@gmail.com>
2023-10-11 22:24:52 +0200

    mas seo

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -7,7 +7,7 @@ export default function Testimonials() {
         <div className="w-full max-w-6xl mx-auto">
           <div className="text-center max-w-xl mx-auto">
             <h1 className="mb-2 mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Que dicen los clientes de mi</h1>
-            <h3 className="text-xl mb-5 font-light">Testimonios reales</h3>
+            <div className="text-xl mb-5 font-light">Testimonios reales</div>
             <div className="text-center mb-10">
               <span className="inline-block w-1 h-1 rounded-full bg-sabagreen-50 ml-1"></span>
               <span className="inline-block w-3 h-1 rounded-full bg-sabagreen-50 ml-1"></span>
